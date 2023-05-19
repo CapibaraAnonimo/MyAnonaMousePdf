3db0af93cceac3e77be9086ff0e078959ab46e4a
CapibaraNonimo <adrianarnaizcano@gmail.com>
2023-05-19 09:27:25 +0200

    Algún pequeño cambio con el refresh token, va, pero no se propagan las excepciones, así que me jodo, Miguel dice que lo deje así, que no importa

--- mobile/packages/myanonamousepdf_api/lib/src/myanonamousepdf_api_client.dart
+++ mobile/packages/myanonamousepdf_api/lib/src/myanonamousepdf_api_client.dart
@@ -26,8 +26,8 @@ class MyanonamousepdfApiClient {
   final box = GetStorage();
 
   //static String _baseUrlApi = "http://localhost:8080/";
-  //static String _baseUrlApi = "http://10.0.2.2:8080/";
-  static String _baseUrlApi = "http://192.168.0.159:8080/";
+  static String _baseUrlApi = "http://10.0.2.2:8080/";
+  //static String _baseUrlApi = "http://192.168.0.159:8080/";
 
   final http.Client _httpClientt;
 
