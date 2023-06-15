3c95c585dc6edd3c94e30b5386f7eca50863b6c5
CapibaraNonimo <adrianarnaizcano@gmail.com>
2023-06-15 10:08:17 +0200

    Small changes

--- mobile/lib/util/globals.dart
+++ mobile/lib/util/globals.dart
@@ -2,4 +2,5 @@ library globals;
 
 //String baseUrlApi = "http://localhost:8080/";
 //String baseUrlApi = "http://10.0.2.2:8080/";
-String baseUrlApi = "http://192.168.0.159:8080/";
+//String baseUrlApi = "http://192.168.0.159:8080/";
+String baseUrlApi = "http://172.27.3.133:8080/";
--- mobile/packages/myanonamousepdf_api/lib/src/myanonamousepdf_api_client.dart
+++ mobile/packages/myanonamousepdf_api/lib/src/myanonamousepdf_api_client.dart
@@ -28,7 +28,8 @@ class MyanonamousepdfApiClient {
 
   //static String _baseUrlApi = "http://localhost:8080/";
   //static String _baseUrlApi = "http://10.0.2.2:8080/";
-  static String _baseUrlApi = "http://192.168.0.159:8080/";
+  //static String _baseUrlApi = "http://192.168.0.159:8080/";
+  static String _baseUrlApi = "http://172.27.3.133:8080/";
 
   final http.Client _httpClientt;
 
